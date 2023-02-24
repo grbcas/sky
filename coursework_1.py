bf376d4ffa9112cf42091eec0d2683e6e462cf57
grbcas <grbcas@gmail.com>
2023-02-24 03:59:49 +0300

    курсовая 1 - создан словарь zip'ом

--- coursework_1.py
+++ coursework_1.py
@@ -1,3 +1,6 @@
+import random
+
+
 MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
                    'C': '-.-.', 'D': '-..', 'E': '.',
                    'F': '..-.', 'G': '--.', 'H': '....',
@@ -15,36 +18,60 @@ MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
                    '(': '-.--.', ')': '-.--.-'}
 
 
-# get input
-def get_input() -> str:
-    input_word = input('word_to_encode: ').upper()
-    return input_word
-
-
-# check if input can be encoded with Morse code
-def check_input(str_to_encode) -> str:
-    if not set(str_to_encode) - set(MORSE_CODE_DICT):
-        return str_to_encode
-    else:
-        print('incorrect symbols: ', *(set(word_to_encode) - set(MORSE_CODE_DICT)))
-
-
-# encoding str into Morse code
 def morse_encode(str_to_encode: str) -> list:
+    """encode str into Morse code list"""
     morse_encoded_symbols = []
-    for symbol in str_to_encode:
+    for symbol in str_to_encode.upper():
         morse_encoded_symbols.append(MORSE_CODE_DICT.get(symbol))
     return morse_encoded_symbols
 
 
+def make_random_tasks(tasks_list: list) -> dict:
+    """make dict task:answer in random order"""
+    input('Сегодня мы потренируемся расшифровывать морзянку.\n'
+          'Нажмите Enter и начнем')
+    # создаём список индексов в случайном порядке в диапазоне от 0..len(tasks_list)
+    random_indexes = random.sample(range(0, len(tasks_list)), len(tasks_list))
+    random_tasks_list = [x for y, x in sorted(zip(random_indexes, tasks_list))]
+    random_answer_list = list(map(morse_encode, random_tasks_list))
+    random_tasks_dict = {}
+    for i_task, i_answer in zip(random_tasks_list, random_answer_list):
+        random_tasks_dict[i_task] = i_answer
+    return random_tasks_dict
+
+
+def get_input() -> str:
+    """get input str"""
+    input_word = input(f'Введите слово: ')
+    return input_word
+
+
+def check(task: str, user_answer: str) -> bool:
+    """check user input"""
+    if user_answer == task:
+        print(f'Верно, {task}!')
+        return True
+    else:
+        print(f'Неверно, {task}!')
+        return False
+
+
+def print_statistics(answers_list: list):
+    """print_statistics"""
+    print(f'Всего задачек: {len(answers_list)}\n'
+          f'Отвечено верно: {answers_list.count(True)}\n'
+          f'Отвечено неверно:{answers_list.count(False)}')
+
+
 if __name__ == '__main__':
+    tasks = ['code', 'bit', 'list', 'soul', 'next']
+    answers = []
     try:
-        while True:
-            word_to_encode = get_input()
-            if check_input(word_to_encode):
-                morse_code = morse_encode(word_to_encode)
-                print(*morse_code, sep=' ')
-            else:
-                continue
+        random_tasks = make_random_tasks(tasks)
+        for i_index, random_task in enumerate(random_tasks):
+            print(f'Слово {i_index + 1}: {" ".join(random_tasks.get(random_task))}')
+            user_input = get_input().lower()
+            answers.append(check(random_task, user_input))
+        print_statistics(answers)
     except KeyboardInterrupt:
-        print('Terminated with KeyboardInterrupt')
+        print('\nTerminated with KeyboardInterrupt')
