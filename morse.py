471767e8c566b9bfaa3e5801c5371e474a5fe7b4
grbcas <grbcas@gmail.com>
2023-02-25 15:01:21 +0300

    урок 4, дз: словари

---
+++ morse.py
@@ -0,0 +1,50 @@
+MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
+                   'C': '-.-.', 'D': '-..', 'E': '.',
+                   'F': '..-.', 'G': '--.', 'H': '....',
+                   'I': '..', 'J': '.---', 'K': '-.-',
+                   'L': '.-..', 'M': '--', 'N': '-.',
+                   'O': '---', 'P': '.--.', 'Q': '--.-',
+                   'R': '.-.', 'S': '...', 'T': '-',
+                   'U': '..-', 'V': '...-', 'W': '.--',
+                   'X': '-..-', 'Y': '-.--', 'Z': '--..',
+                   '1': '.----', '2': '..---', '3': '...--',
+                   '4': '....-', '5': '.....', '6': '-....',
+                   '7': '--...', '8': '---..', '9': '----.',
+                   '0': '-----', ', ': '--..--', '.': '.-.-.-',
+                   '?': '..--..', '/': '-..-.', '-': '-....-',
+                   '(': '-.--.', ')': '-.--.-'}
+
+
+def get_input() -> str:
+    input_word = input('word_to_encode: ').upper()
+    return input_word
+
+
+# get input and check if it can be encoded with Morse code
+def check_input(str_to_encode) -> str:
+    if not set(str_to_encode) - set(MORSE_CODE_DICT):
+        return str_to_encode
+    else:
+        print('incorrect', *(set(word_to_encode) - set(MORSE_CODE_DICT)))
+
+
+# encoding str into Morse code
+def morse_encode(str_to_encode: str) -> list:
+    print(str_to_encode)
+    morse_encoded_symbols = []
+    for symbol in str_to_encode:
+        morse_encoded_symbols.append(MORSE_CODE_DICT.get(symbol))
+    return morse_encoded_symbols
+
+
+if __name__ == '__main__':
+    try:
+        while True:
+            word_to_encode = get_input()
+            if check_input(word_to_encode):
+                morse_code = morse_encode(word_to_encode)
+                print(*morse_code, sep=' ')
+            else:
+                continue
+    except KeyboardInterrupt:
+        print('Terminated with KeyboardInterrupt')
---
+++ task_lesson_4-str_dict.py
@@ -0,0 +1,81 @@
+
+def choose_difficulty_level() -> dict:
+    words_easy = {
+        "family": "семья",
+        "hand": "рука",
+        "people": "люди",
+        "evening": "вечер",
+        "minute": "минута",
+    }
+    words_medium = {
+        "believe": "верить",
+        "feel": "чувствовать",
+        "make": "делать",
+        "open": "открывать",
+        "think": "думать",
+    }
+    words_hard = {
+        "rural": "деревенский",
+        "fortune": "удача",
+        "exercise": "упражнение",
+        "suggest": "предлагать",
+        "except": "кроме",
+    }
+    difficulty_level = {}
+    difficulty_levels = {'легкий': words_easy, 'средний': words_medium, 'сложный': words_hard}
+    level_input = input('Выберите уровень сложности.\nЛегкий, средний, сложный. ').lower()
+    if level_input in difficulty_levels:
+        difficulty_level = difficulty_levels.get(level_input)
+    else:
+        print("Задан несуществующий уровень. Выход")
+        exit()
+    print(f'Выбран уровень сложности, мы предложим {len(difficulty_level)} слов, подберите перевод.')
+    return difficulty_level
+
+
+def get_answers(words_dict: dict) -> dict:
+    answers = {}
+    for key, val in words_dict.items():
+        print(f'{key}, {len(val)} букв,  начинается на {val[0]}...')
+        answer = input('Ответ: ').lower()
+        print(key, answer)
+        if val == answer:
+            print(f'Верно, {key} — это {val}.')
+            answers[key] = True
+        else:
+            print(f'Неверно, {key} — это {val}.')
+            answers[key] = False
+    return answers
+
+
+def score_result(answer_dict: dict):
+    levels = {
+        0: "Нулевой",
+        1: "Так себе",
+        2: "Можно лучше",
+        3: "Норм",
+        4: "Хорошо",
+        5: "Отлично",
+    }
+    mistakes = []
+    print('Правильно отвечены слова: ')
+    for key, val in answer_dict.items():
+        if val:
+            print(key)
+        else:
+            mistakes.append(key)
+
+    print('Неправильно отвечены слова:')
+    [print(x) for x in mistakes]
+    user_rang = len(answer_dict) - len(mistakes)
+    print(f'Ваш ранг:\n{levels.get(user_rang)}')
+
+
+if __name__ == '__main__':
+    words = choose_difficulty_level()
+    result = get_answers(words)
+    score_result(result)
+
+
+
+
