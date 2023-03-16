5d71dca70434214311e42b48c12837465798dbe0
grbcas <grbcas@gmail.com>
2023-03-16 17:44:26 +0300

    task 2.1

---
+++ task 2.1.py
@@ -0,0 +1,42 @@
+from random import random
+
+
+def shuffle_word(word) -> str:
+	lst_to_shuffle = list(word.rstrip())
+	r = sorted(lst_to_shuffle, key=lambda x: random())
+	s_word = ''.join(r)
+	return s_word
+
+
+def main():
+	name = input('Введите ваше имя: ')
+	with open('words.txt', 'r', encoding="utf-8") as f_in:
+		words = f_in.readlines()
+	score = 0
+	for word in words:
+		print('Угадайте слово: ', shuffle_word(word))
+		user_word = input('> ').lower()
+		if user_word == word.rstrip():
+			score += 10
+			print(f'Верно! Вы получаете 10 очков.')
+		else:
+			print(f'Неверно! Верный ответ – {word.rstrip()}.')
+
+	with open('history.txt', 'a', encoding="utf-8") as f_out:
+		print(name, str(score))
+		f_out.write(name + ' ' + str(score) + '\n')
+
+	with open('history.txt', 'r', encoding="utf-8") as f:
+		games = 0
+		record = 0
+		for line in f:
+			games += 1
+			result = line.rstrip().split()
+			if len(result) > 1:
+				record += int(result[1])
+
+		print('\nВсего игр сыграно: ', games)
+		print('Максимальный рекорд: ', record)
+
+
+main()
