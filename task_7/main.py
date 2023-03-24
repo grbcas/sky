ce88710fee00b77460f5628bc6074a2e89bae868
grbcas <grbcas@gmail.com>
2023-03-24 23:04:58 +0300

    task 7.2 - Погружение: коллекции и вложенность. #3

--- task_7/main.py
+++ task_7/main.py
@@ -5,16 +5,19 @@ def main():
 	pk = int(input('Введите номер студента: '))
 	student = get_student_by_pk(pk)
 
+	# Выводим информации о студенте
 	student_name = ''.join([x for x in student.keys()])
 	print(student_name)
 	student_skills = [x for x in student.values()][0]
 	print(f'Знает: {", ".join(student_skills)}')
 
+	# Пользовательский ввод специальности для оценки студента
 	title = input(f'Выберите специальность для оценки студента {student_name}: ').capitalize()
+	# Загружаем информацию о навыках указанной профессии
 	profession = (get_profession_by_title(title).get(title))
 
+	# Загружаем информацию о том, что знает/не знает студент и степенью его пригодности
 	result = check_fitness(student_skills, profession)
-	print(result)
 	print('Пригодность:', result['fit_percent'])
 	print('Студент знает:', ', '.join(result['has']))
 	print('Студент не знает:', ', '.join(result['lacks']))
